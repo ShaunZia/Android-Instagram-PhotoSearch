c786254b19059e22d69e9ab52bc09ed27afc255a
ShaunZia <shaunzia2@yahoo.ca>
2017-09-12 20:33:21 -0700

    Added Android Instagram image viewer

---
+++ android-instagram-photosearch/app/src/main/java/com/tag/instagramdemo/example/ApplicationData.java
@@ -0,0 +1,14 @@
+/*
+By Shaun Zia
+Completed October 12, 2015
+For Stradigi Inc.
+Define Instagram client access
+ */
+
+package com.tag.instagramdemo.example;
+
+public class ApplicationData {
+	public static final String CLIENT_ID = "382421534e1143e094f10243276a58d3";
+	public static final String CLIENT_SECRET = "144abf908fd949ac87fc26da0515e3c7";
+	public static final String CALLBACK_URL = "http://www.photosearch.com";
+}
